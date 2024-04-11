9e9d31771f899a523f181aefb00d822f5daaf4f0
eleidyy <163553570+eleidyy@users.noreply.github.com>
2024-04-11 17:01:29 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,11 @@
 def check_vowels():
     # Código a implementar utilizando input.
+txt = input("Ingrese su nombre: ")
+print("a" in txt)
+print("e" in txt)
+print("i" in txt)
+print("o" in txt)
+print("u" in txt)
 
 
 # Para verificar este ejercicio ejecutar el comando
