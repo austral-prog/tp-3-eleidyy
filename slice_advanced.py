3711d6b03f7a6aaf0ccb28c33a095da92eef7a76
eleidyy <163553570+eleidyy@users.noreply.github.com>
2024-04-11 17:34:31 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,8 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-
+texto = "Awesome"
+print(texto[0:3].lower())
+print(texto[len(texto)//2-1:len(texto)//2+2].lower())
+print(texto[0:4].lower() + texto[-2:].lower())
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
